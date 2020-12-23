7887f9c31d50b839210dca363e1c0a6b77208b76
Peter Seibel <peter@gigamonkeys.com>
2020-12-23 14:25:48 -0800

    Day 8, part 1.

--- day-08.py
+++ day-08.py
@@ -13,6 +13,21 @@ class Op:
     num: int
 
 
+@dataclass
+class VM:
+    pc: int = 0
+    acc: int = 0
+
+    def execute(self, op):
+        if op.code == "acc":
+            self.acc += op.num
+            self.pc += 1
+        elif op.code == "jmp":
+            self.pc += op.num
+        elif op.code == "nop":
+            self.pc += 1
+
+
 def parse(input):
     return [parse_op(line[:-1]) for line in input]
 
@@ -24,8 +39,17 @@ def parse_op(line):
         raise Exception(f"{line} doesn't match line pattern")
 
 
+def run(ops):
+    vm = VM()
+    seen = set()
+    while vm.pc not in seen:
+        seen.add(vm.pc)
+        vm.execute(ops[vm.pc])
+    return vm.acc
+
+
 if __name__ == "__main__":
 
-    instructions = parse(fileinput.input())
+    ops = parse(fileinput.input())
 
-    print(instructions)
+    print(run(ops))
