4d369fed06780c53fe62c4d4703c03b01ffe544d
Peter Seibel <peter@gigamonkeys.com>
2020-12-22 19:47:08 -0800

    Whoops. Day 2, part 2.

--- day-02.py
+++ day-02.py
@@ -7,6 +7,9 @@ import re
 
 pat = re.compile(r"^(\d+)-(\d+) (\w): (\w+)$")
 
+def xor(a, b):
+    return (not a and b) or (a and not b)
+
 @dataclass
 class Line:
     min: int
@@ -18,18 +21,18 @@ class Line:
         return self.min <= sum(self.char == c for c in self.password) <= self.max
 
     def ok2(self):
-        return sum(self.char == c for c in self.password[self.min - 1:self.max]) == 1
-
-
+        return xor(self.char == self.password[self.min - 1], self.char == self.password[self.max - 1])
 
 
 def parse_file(lines):
     for line in lines:
         if m := pat.match(line):
             yield Line(int(m.group(1)), int(m.group(2)), m.group(3), m.group(4))
+        else:
+            print(f"No match: {line}")
 
 
 if __name__ == "__main__":
 
-    print(sum(line.ok() for line in parse_file(fileinput.input())))
+    #print(sum(line.ok() for line in parse_file(fileinput.input())))
     print(sum(line.ok2() for line in parse_file(fileinput.input())))
