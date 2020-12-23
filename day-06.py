9a4ff4bb4a2964e1c6b9335460b6acf83fd1316d
Peter Seibel <peter@gigamonkeys.com>
2020-12-23 11:23:40 -0800

    Day 6, part 2.

--- day-06.py
+++ day-06.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import fileinput
+from functools import reduce
 
 
 def parse(input):
@@ -8,19 +9,27 @@ def parse(input):
 
 
 def stanzas(input):
-    buf = ""
+    buf = []
     for line in input:
         if line[:-1]:
-            buf += line[:-1]
+            buf.append(set(line[:-1]))
         else:
             yield buf
-            buf = ""
+            buf = []
     if buf:
         yield buf
 
 
-def total(answers):
-    return sum(len(set(x)) for x in answers)
+def anyone(x):
+    return len(reduce(lambda a, b: a | b, x))
+
+
+def everyone(x):
+    return len(reduce(lambda a, b: a & b, x))
+
+
+def total(answers, fn=anyone):
+    return sum(fn(x) for x in answers)
 
 
 if __name__ == "__main__":
@@ -28,3 +37,4 @@ if __name__ == "__main__":
     answers = parse(fileinput.input())
 
     print(total(answers))
+    print(total(answers, everyone))
