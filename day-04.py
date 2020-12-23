acfe65ae9fb71eafab62c087ff75fae7056ac2ec
Peter Seibel <peter@gigamonkeys.com>
2020-12-22 21:26:02 -0800

    Day 4, part 1.

--- day-04.py
+++ day-04.py
@@ -2,6 +2,19 @@
 
 import fileinput
 
+fields = {
+    "byr": True,
+    "iyr": True,
+    "eyr": True,
+    "hgt": True,
+    "hcl": True,
+    "ecl": True,
+    "pid": True,
+    "cid": False,
+}
+
+required = {k for k, v in fields.items() if v}
+
 
 def parse(input):
     return list(stanzas(input))
@@ -23,7 +36,10 @@ def to_dict(x):
     return dict(p.split(":") for p in x)
 
 
+def ok(passport):
+    return not (required - set(passport))
+
+
 if __name__ == "__main__":
 
-    x = parse(fileinput.input())
-    print(x)
+    print(sum(ok(p) for p in parse(fileinput.input())))
