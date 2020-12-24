9a7328a6a11797bc14fd6f773286e2b9301aa861
Peter Seibel <peter@gigamonkeys.com>
2020-12-23 16:42:40 -0800

    Day 8, part 2.

--- day-08.py
+++ day-08.py
@@ -4,7 +4,10 @@ import fileinput
 import re
 from dataclasses import dataclass
 from dataclasses import field
-from typing import Set
+from dataclasses import replace
+from typing import FrozenSet
+from typing import List
+from typing import Optional
 
 op = re.compile("(\w+) ([-+]\d+)")
 
@@ -17,22 +20,50 @@ class Op:
 
 @dataclass
 class VM:
+    ops: List[Op]
     pc: int = 0
     acc: int = 0
-    seen: Set[int] = field(default_factory=set)
+    done: bool = False
+    looped: bool = False
+    patch: Optional[int] = None
+    seen: FrozenSet[int] = field(default_factory=lambda: frozenset(set()))
 
-    def looped(self):
-        return self.pc in self.seen
+    def execute(self):
 
-    def execute(self, op):
-        self.seen.add(self.pc)
+        if self.pc in self.seen:
+            self.done = True
+            self.looped = True
+            return
+
+        if self.pc == len(self.ops):
+            self.done = True
+            return
+
+        self.seen |= {self.pc}
+
+        op = self.ops[self.pc]
+
+        if self.patch == self.pc:
+            self.execute_patched(op)
+        else:
+            if op.code == "acc":
+                self.acc += op.num
+                self.pc += 1
+            elif op.code == "jmp":
+                self.pc += op.num
+            elif op.code == "nop":
+                self.pc += 1
+
+    def execute_patched(self, op):
         if op.code == "acc":
-            self.acc += op.num
-            self.pc += 1
+            raise Exception(f"Can't patch at acc pc: {self.pc}")
         elif op.code == "jmp":
-            self.pc += op.num
-        elif op.code == "nop":
             self.pc += 1
+        elif op.code == "nop":
+            self.pc += op.num
+
+    def patched(self):
+        return replace(self, patch=self.pc)
 
 
 def parse(input):
@@ -46,15 +77,28 @@ def parse_op(line):
         raise Exception(f"{line} doesn't match line pattern")
 
 
-def run(ops):
-    vm = VM()
-    while not vm.looped():
-        vm.execute(ops[vm.pc])
-    return vm.acc
+def run(vm):
+    while not vm.done:
+        vm.execute()
+    return vm.acc, vm.looped
+
+
+def find_patch(ops):
+    vm = VM(ops)
+
+    while not vm.looped:
+        vm.execute()
+        if ops[vm.pc].code in {"jmp", "nop"}:
+            p = vm.patched()
+            acc, looped = run(p)
+            if not looped:
+                return acc
+    return None
 
 
 if __name__ == "__main__":
 
     ops = parse(fileinput.input())
 
-    print(run(ops))
+    # print(run(VM(ops)))
+    print(find_patch(ops))
