f66819643c67341f94d1e404c76b753617a4c8ef
Peter Seibel <peter@gigamonkeys.com>
2020-12-24 15:23:41 -0800

    Day 13, part 1.

--- day-13.py
+++ day-13.py
@@ -2,13 +2,21 @@
 
 import fileinput
 
+
 def parse(input):
     lines = list(input)
     return int(lines[0][:-1]), [int(n) for n in lines[1][:-1].split(",") if n != "x"]
 
 
+def shortest_wait(start, buses):
+    return min((b - (start % b), b) for b in buses)
+
+
 if __name__ == "__main__":
 
-    sched = parse(fileinput.input())
+    start, buses = parse(fileinput.input())
 
-    print(sched)
+    wait, bus = shortest_wait(start, buses)
+
+    # print((wait, bus))
+    print(wait * bus)
