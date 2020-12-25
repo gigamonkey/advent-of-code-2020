ece45872168c58508808ca0371c614e2efd0acb2
Peter Seibel <peter@gigamonkeys.com>
2020-12-24 23:03:38 -0800

    Day 16, part 1.

--- day-16.py
+++ day-16.py
@@ -40,9 +40,9 @@ def parse(input):
                 raise Exception("Expecting rule.")
 
         elif state == "yours":
-            ticket = line.split(",")
+            ticket = parse_ticket(line)
         elif state == "nearby":
-            nearby.append(line.split(","))
+            nearby.append(parse_ticket(line))
 
     return Notes(rules, ticket, nearby)
 
@@ -55,6 +55,20 @@ def parse_rule(s):
     return [to_range(r) for r in s.split(" or ")]
 
 
+def parse_ticket(s):
+    return [int(x) for x in s.split(",")]
+
+
+def ticket_scanning_error_rate(rules, tickets):
+    return sum(n for t in tickets for n in t if not valid(rules, n))
+
+
+def valid(rules, n):
+    return any(n in r for rule in rules.values() for r in rule)
+
+
 if __name__ == "__main__":
 
-    print(parse(fileinput.input()))
+    notes = parse(fileinput.input())
+
+    print(ticket_scanning_error_rate(notes.rules, notes.nearby))
