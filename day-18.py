6a027a2d39308316b156aecebe9be2aaabe4412f
Peter Seibel <peter@gigamonkeys.com>
2020-12-25 13:23:43 -0800

    Day 18, brute force parser.

--- day-18.py
+++ day-18.py
@@ -17,6 +17,9 @@ from typing import Optional
 # 1 + 2 * 3 + 4 * 5 + 6 => (((((1 + 2) * 3) + 4) * 5) + 6)
 
 
+ops = {"+": add, "*": mul}
+
+
 @dataclass
 class Expression:
     pass
@@ -44,27 +47,65 @@ def parse(input):
     return [parse_expr(line[:-1]) for line in input]
 
 
-def parse_expr(s):
-    pass
+def parse_expr(s, pos=0):
+
+    # Read first expression (value or parenthesized)
+    left, pos = expr(s, pos)
+
+    while pos < len(s):
+        # Read the op
+        fn, pos = op(s, pos)
+
+        # Read next expression (value or parenthesized)
+        right, pos = expr(s, pos)
+
+        left = Binary(fn, left, right)
+        if pos < len(s) and s[pos] == ")":
+            return left, eat_whitespace(pos + 1)
+
+    return left, len(s)
+
+
+def expr(s, pos):
+    if s[pos] == "(":
+        return parse_expr(s, pos + 1)
+    else:
+        buf = ""
+        while pos < len(s) and s[pos].isdigit():
+            buf += s[pos]
+            pos += 1
+        return Value(int(buf)), eat_whitespace(s, pos)
+
+
+def op(s, pos):
+    return ops[s[pos]], eat_whitespace(s, pos + 1)
+
+
+def eat_whitespace(s, pos):
+    while pos < len(s) and s[pos] == " ":
+        pos += 1
+    return pos
 
 
 if __name__ == "__main__":
 
     # 1 + 2 * 3 + 4 * 5 + 6
 
-    expr = Binary(
-        add,
+    print(
         Binary(
-            mul,
+            add,
             Binary(
-                add, Binary(mul, Binary(add, Value(1), Value(2)), Value(3)), Value(4)
+                mul,
+                Binary(
+                    add,
+                    Binary(mul, Binary(add, Value(1), Value(2)), Value(3)),
+                    Value(4),
+                ),
+                Value(5),
             ),
-            Value(5),
-        ),
-        Value(6),
+            Value(6),
+        ).eval()
     )
-
-    print(expr.eval())
 
     v1 = Value(1)
     b1 = Binary(add, v1)
@@ -84,3 +125,25 @@ if __name__ == "__main__":
     b5.right = v6
 
     print(b5.eval())
+
+    left = Value(1)
+    b = Binary(add, left)
+    right = Value(2)
+    b1.right = right
+    b2 = Binary(mul, b1)
+    v3 = Value(3)
+    b2.right = v3
+    b3 = Binary(add, b2)
+    v4 = Value(4)
+    b3.right = v4
+    b4 = Binary(mul, b3)
+    v5 = Value(5)
+    b4.right = v5
+    b5 = Binary(add, b4)
+    v6 = Value(6)
+    b5.right = v6
+
+    print(b5.eval())
+
+    tree, pos = parse_expr("1 + 2 * 3 + 4 * 5 + 6")
+    print(tree.eval())
