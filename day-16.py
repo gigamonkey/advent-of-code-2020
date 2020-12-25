3f1965f6939f464e2b8af840aaf555a7ce8d52cd
Peter Seibel <peter@gigamonkeys.com>
2020-12-24 23:28:21 -0800

    Day 16, search.

--- day-16.py
+++ day-16.py
@@ -67,8 +67,56 @@ def valid(rules, n):
     return any(n in r for rule in rules.values() for r in rule)
 
 
+def valid_ticket(rules, ticket):
+    return all(valid(rules, n) for n in ticket)
+
+
+def possible_field(rules, field, value):
+    return any(value in r for r in rules[field])
+
+
+def deduce_fields(notes):
+
+    possible_fields = frozenset(notes.rules.keys())
+
+    possibilities = [possible_fields] * len(notes.rules)
+
+    # Search: For each ticket, for each position in the ticket check
+    # the value against the rule for the fields that are still
+    # possibilities for that field and eliminate any field that is
+    # violated by the value. If any position gets whittled down to a
+    # single possibile field, remove that field from all other
+    # positions. A simple search since we don't need to worry about
+    # backtracking.
+
+    for ticket in notes.nearby:
+        if valid_ticket(notes.rules, ticket):
+            for i, v in enumerate(ticket):
+                for field in possibilities[i]:
+                    if not possible_field(notes.rules, field, v):
+                        if eliminate(possibilities, i, field):
+                            return [list(p)[0] for p in possibilities]
+
+    raise Exception(f"No solution: {possibilities}")
+
+
+def eliminate(possibilities, i, field):
+    if field in possibilities[i]:
+        possibilities[i] = possibilities[i] - {field}
+        if len(possibilities[i]) == 1:
+            for j in range(len(possibilities)):
+                if j != i:
+                    eliminate(possibilities, j, list(possibilities[i])[0])
+    return all(len(p) == 1 for p in possibilities)
+
+
 if __name__ == "__main__":
 
     notes = parse(fileinput.input())
 
-    print(ticket_scanning_error_rate(notes.rules, notes.nearby))
+    part = 2
+
+    if part == 1:
+        print(ticket_scanning_error_rate(notes.rules, notes.nearby))
+    else:
+        print(deduce_fields(notes))
