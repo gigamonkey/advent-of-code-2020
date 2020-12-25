958496b736a9ec246665a6125b0b95fa3bd72b35
Peter Seibel <peter@gigamonkeys.com>
2020-12-25 15:14:34 -0800

    Day 18, part 2 works for simple cases.

--- day-18.py
+++ day-18.py
@@ -7,16 +7,7 @@ from operator import mul
 from typing import Callable
 from typing import Optional
 
-# 1 + 2 * 3 + 4 * 5 + 6
-#   3   * 3 + 4 * 5 + 6
-#       9   + 4 * 5 + 6
-#          13   * 5 + 6
-#              65   + 6
-#                  71
-
-
 # 1 + 2 * 3 + 4 * 5 + 6 => (((((1 + 2) * 3) + 4) * 5) + 6)
-
 
 ops = {"+": add, "*": mul}
 
@@ -44,10 +35,6 @@ class Binary(Expression):
         return self.op(self.left.eval(), self.right.eval())
 
 
-def parse(input):
-    return [(line[:-1], parse_expr(line[:-1])[0]) for line in input]
-
-
 def parse_expr(s, pos=0):
 
     # Read first expression (value or parenthesized)
@@ -65,6 +52,10 @@ def parse_expr(s, pos=0):
             return left, eat_whitespace(s, pos + 1)
 
     return left, len(s)
+
+
+def parse(input, fn=parse_expr):
+    return [(line[:-1], fn(line[:-1], 0)[0]) for line in input]
 
 
 def expr(s, pos):
@@ -88,6 +79,74 @@ def eat_whitespace(s, pos):
     return pos
 
 
+#
+# Part 2
+#
+
+
+def addition(s, pos):
+
+    # Number of parenthesized expression
+    left, pos = term(s, pos)
+
+    while pos < len(s) and s[pos] == "+":
+        fn, pos = op(s, pos)
+
+        # Next number or parenthesized expression
+        right, pos = term(s, pos)
+
+        left = Binary(fn, left, right)
+        if pos < len(s) and s[pos] == ")":
+            return left, eat_whitespace(s, pos + 1)
+
+    return left, eat_whitespace(s, pos)
+
+
+def product(s, pos):
+
+    # Number, parenthesized expression, or an addition
+    left, pos = addition(s, pos)
+
+    while pos < len(s) and s[pos] == "*":
+        fn, pos = op(s, pos)
+
+        # Next number, parenthesized expression, or addition
+        right, pos = addition(s, pos)
+
+        left = Binary(fn, left, right)
+        if pos < len(s) and s[pos] == ")":
+            return left, eat_whitespace(s, pos + 1)
+
+    return left, eat_whitespace(s, pos)
+
+
+def term(s, pos):
+    if s[pos] == "(":
+        return product(s, pos + 1)
+    else:
+        buf = ""
+        while pos < len(s) and s[pos].isdigit():
+            buf += s[pos]
+            pos += 1
+        return Value(int(buf)), eat_whitespace(s, pos)
+
+
+def factor(s, pos):
+    "A thing on either side of a *. Either a value, a parenthesized expression, or a sum."
+    if s[pos] == "(":
+        return parse_expr(s, pos + 1)
+    else:
+        buf = ""
+        while pos < len(s) and s[pos].isdigit():
+            buf += s[pos]
+            pos += 1
+        return Value(int(buf)), eat_whitespace(s, pos)
+
+
 if __name__ == "__main__":
 
-    print(sum(x[1].eval() for x in parse(fileinput.input())))
+    part = 2
+
+    fn = parse_expr if part == 1 else product
+
+    print(sum(x[1].eval() for x in parse(fileinput.input(), fn)))
