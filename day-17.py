817dc62948b847cca03d45098d9d1a6bcdeba94a
Peter Seibel <peter@gigamonkeys.com>
2020-12-25 11:41:54 -0800

    Day 17, part 2.

--- day-17.py
+++ day-17.py
@@ -5,33 +5,34 @@ from itertools import product
 
 d = (-1, 0, 1)
 
-cube = tuple(product(d, d, d))
+dims = {dim: tuple(product(d, repeat=dim)) for dim in (3, 4)}
 
 
-def parse(input):
+def parse(input, dims=3):
     on = set()
     for y, row in enumerate(input):
         for x, c in enumerate(row):
             if c == "#":
-                on.add((x, y, 0))
+                on.add((x, y) + (0,) * (dims - 2))
     return on
 
 
-def cube_at(coord):
-    return {tuple(c + d for c, d in zip(coord, n)) for n in cube}
+def at(coord):
+    return {tuple(c + d for c, d in zip(coord, n)) for n in dims[len(coord)]}
 
 
 def step(state):
-    to_consider = {p for on in state for p in cube_at(on)}
+    to_consider = {p for on in state for p in at(on)}
     return {p for p in to_consider if active_in_next(p, state)}
 
 
 def active_in_next(coord, state):
-    on = len(cube_at(coord) & state)
+    on = len(at(coord) & state)
     return on == 3 or (coord in state and on == 4)
 
 
 def dump(state):
+    # Only works for 3d
     xs = [p[0] for p in state]
     ys = [p[1] for p in state]
     zs = [p[2] for p in state]
@@ -48,19 +49,13 @@ def dump(state):
 
 if __name__ == "__main__":
 
-    initial = parse(fileinput.input())
-
-    print(initial)
+    initial = parse(fileinput.input(), dims=4)
 
     c = 0
-    print(f"After {c} cycle(s)")
     state = initial
-    dump(state)
 
     for i in range(6):
         c += 1
         state = step(state)
-        print(f"After {c} cycle(s)")
-        dump(state)
 
     print(len(state))
