fec680ed3d50a555837e1f902ba9533e5bb9c214
Peter Seibel <peter@gigamonkeys.com>
2020-12-26 09:03:05 -0800

    Day 20, edge extraction.

--- day-20.py
+++ day-20.py
@@ -3,10 +3,36 @@
 # Rotations - 0, 90, 180, 270 degrees.
 # Flips - horizontal, vertical, two diagonals (?)
 
-import re
 import fileinput
+import re
+from dataclasses import dataclass
+from typing import List
 
 tile_pat = re.compile("^Tile (\d+):")
+
+
+@dataclass
+class Tile:
+
+    num: int
+    rows: List[str]
+
+    @property
+    def top(self):
+        return self.rows[0]
+
+    @property
+    def bottom(self):
+        return self.rows[-1]
+
+    @property
+    def left(self):
+        return "".join(row[0] for row in self.rows)
+
+    @property
+    def right(self):
+        return "".join(row[-1] for row in self.rows)
+
 
 def parse(input):
     tiles = {}
@@ -21,18 +47,22 @@ def parse(input):
                     rows.append(text)
                 else:
                     break
-            tiles[num] = rows
+            tiles[num] = Tile(num, rows)
 
     return tiles
-
 
 
 if __name__ == "__main__":
 
     tiles = parse(fileinput.input())
 
-    for num, tile in tiles.items():
-        print(f"Tile {num}:")
-        for row in tile:
+    for tile in tiles.values():
+        print(f"Tile {tile.num}:")
+        for row in tile.rows:
             print(row)
+        print()
+        print(f"Top: {tile.top}")
+        print(f"Bottom: {tile.bottom}")
+        print(f"Left: {tile.left}")
+        print(f"Right: {tile.right}")
         print()
