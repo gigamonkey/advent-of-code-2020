5fd3cda34847ea100e547777dd66b8c7fe469a78
Peter Seibel <peter@gigamonkeys.com>
2020-12-28 17:28:01 -0800

    Day 24, part 2.

--- day-24.py
+++ day-24.py
@@ -47,8 +47,35 @@ def black(all_moves):
     return sum(v % 2 == 1 for v in c.values())
 
 
+def neighbors(tile):
+    x, y = tile
+    return [(x + dx, y + dy) for dx, dy in directions.values()]
+
+
+def start(instructions):
+    c = flips(instructions)
+    return {tile for tile, v in c.items() if v % 2 == 1}
+
+
+def step(black):
+    to_consider = {n for b in black for n in neighbors(b)}
+    return {tile for tile in to_consider if is_black_next(tile, black)}
+
+
+def is_black_next(tile, black):
+    black_neighbors = sum(n in black for n in neighbors(tile))
+    return black_neighbors in {1, 2} if tile in black else black_neighbors == 2
+
+
 if __name__ == "__main__":
 
     instructions = parse(fileinput.input())
 
-    print(black(instructions))
+    state = start(instructions)
+
+    for i in range(101):
+        if i < 10 or i % 10 == 0:
+            print(f"Day {i}: {len(state)}")
+        if i == 10:
+            print()
+        state = step(state)
